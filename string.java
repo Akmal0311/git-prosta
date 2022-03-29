9fa961f9472fbfb40eb8a3a1e162241bf62b61c9
Akmal <akmalkomiljonov26@gmail.com>
2022-03-29 08:42:27 +0500

    String for java

---
+++ string.java
@@ -0,0 +1,20 @@
+package JavaCourse;
+
+import java.util.Locale;
+
+public class string {
+    public static void main(String[] args) {
+        String name = "Bro";
+
+        //boolean result = name.equalsIgnoreCase("bro");
+        //int result = name.length();
+        //char result = name.charAt(2);
+        //int result = name.indexOf("r");
+        //boolean result = name.isEmpty();
+        //String result = name.toUpperCase();
+        //String result = name.toLowerCase();
+        //String result = name.replace('r','R');
+
+        //System.out.println(result);
+    }
+}
